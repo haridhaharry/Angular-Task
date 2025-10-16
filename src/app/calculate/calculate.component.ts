102b5ff6a80842f62d9885c06d7248cde6c49f87
haridha <haridhasowmi99@gmail.com>
2025-10-16 07:55:39 +0530

    Before keybroad input va;ue

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -12,14 +12,27 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class CalculateComponent {
 
-calcval : string = '';
-displayval : string = '';
+calcval : any ='';
+subcalcval : any ='';
+nextcalcval : boolean = false;
+// performcalc : boolean = false;
+displayval : any ='';
 items = ['AC','%','backspace','/','7','8','9','*','4','5','6','-','1','2','3','+','00','0','.','='];
+isEqual : boolean = false;
+operator : any;
+operatorset : boolean = false;
+var_1 : number = 0;
+var_2 : number = 0;
 
 OnDisplay(clckval: string){
     if(clckval == '='){
-      this.calcval = eval(this.displayval);
+      this.calcval=this.GetTotalValue();
+      this.isEqual = true;
+      this.var_1 = 0;
+      this.var_2 = 0;
+      this.operatorset = false;
     }
+    
     else if(clckval == 'AC'){
       this.displayval = '';
       this.calcval = '';
@@ -27,9 +40,69 @@ OnDisplay(clckval: string){
     else if(clckval == 'backspace'){
       this.displayval = this.displayval.slice(0, -1);
     }
-    else{
+    else if(this.calcval !== null && this.calcval !== "" && this.isEqual){
+      this.displayval = this.calcval;
+      this.displayval += clckval; 
+      this.isEqual = false;
+    } 
+    else {
       this.displayval += clckval; 
     }
   }
 
+GetTotalValue() {
+
+    for (let i=0; i<(this.displayval.length); i++) {
+          if(this.displayval[i] == '+' || this.displayval[i] == '-' || this.displayval[i] == '*' || this.displayval[i] == '/') {
+            if (this.operatorset) {
+              this.nextcalcval = true;
+              this.PerformCalc();
+              // break;
+              this.var_1 = this.subcalcval;
+              this.var_2 = 0;
+              this.operator = this.displayval[i];
+            }
+            else {
+              this.operator = this.displayval[i];
+              this.operatorset = true;
+            } 
+          }
+          else if (this.nextcalcval) {
+            this.var_2 += this.displayval[i];
+          }
+          else {
+            if(!this.operatorset) {
+              this.var_1 += this.displayval[i];
+            }
+            else {
+              this.var_2 += this.displayval[i];
+            }
+          }
+        this.PerformCalc();
+        this.nextcalcval = false;
+      }   
+  return this.subcalcval;
+}
+
+PerformCalc() {
+  switch (this.operator) {
+    case '+' :
+      this.subcalcval = Number(this.var_1) + Number(this.var_2);
+      break;
+    case '-' :
+      this.subcalcval = this.var_1 - this.var_2;
+      break;
+    case '*' :
+      this.subcalcval = this.var_1 * this.var_2;
+      break;
+    case '/' :
+      this.subcalcval = this.var_1 / this.var_2;
+      break;
+
+    default:
+      break;
+  }
+  // this.performcalc = true;
+  return this.subcalcval;
+  }
 }
