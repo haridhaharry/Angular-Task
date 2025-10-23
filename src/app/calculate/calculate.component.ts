2d56e0fd81713bdbdcaf035cc6915db89c979342
haridha <haridhasowmi99@gmail.com>
2025-10-23 09:56:21 +0530

    Issues fixed

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculate',
   standalone: true,
-  imports: [MatCardModule, MatIconModule, CommonModule],
+  imports: [MatCardModule, MatIconModule, CommonModule, FormsModule],
   templateUrl: './calculate.component.html',
   styleUrl: './calculate.component.scss'
 })
@@ -23,9 +24,46 @@ operator : any;
 operatorset : boolean = false;
 var_1 : number = 0;
 var_2 : number = 0;
+operator_1 : boolean = false;
+operator_2 : boolean = false;
+
+onKeyPress(event: KeyboardEvent) {
+  const allowedKeys = ['Backspace', 'Enter'];
+  const numOrOperator = /^[0-9+\-*/%.]$/.test(event.key);
+
+  if (!numOrOperator && !allowedKeys.includes(event.key)) {
+    event.preventDefault();
+  } else {
+    this.OnDisplay(event.key);
+  }
+}
 
 OnDisplay(clckval: string){
-    if(clckval == '='){
+  const expression = this.calcval;
+  if(this.displayval.length >= 20) {
+      this.displayval = this.displayval.slice(0, 20);
+  }
+  const arthval = ['+', '-', '*', '/', '%'];
+  this.operator_1 = arthval.some(arth => clckval.includes(arth));
+    if(this.operator_1) {
+      // this.displayval.length.includes(arthval);
+      this.operator_2 = arthval.some(arth => this.displayval.slice(-1).includes(arth));
+      if (this.operator_2) {
+        return;
+      }
+      if(this.displayval == null || this.displayval == "") {
+        return;
+      }
+    }
+   
+    if(clckval == '=' || clckval == 'Enter'){
+      if(this.displayval.slice(-1) == '%') {
+        this.calcval=this.GetTotalValue();
+      }
+      this.operator_2 = arthval.some(arth => this.displayval.slice(-1).includes(arth));
+      if (this.operator_2 || this.displayval.slice(-1) == '=' || this.displayval.slice(-1) == 'Enter') {
+        return;
+      }
       this.calcval=this.GetTotalValue();
       this.isEqual = true;
       this.var_1 = 0;
@@ -37,8 +75,11 @@ OnDisplay(clckval: string){
       this.displayval = '';
       this.calcval = '';
     }
-    else if(clckval == 'backspace'){
+    else if(clckval == 'backspace' || clckval == 'Backspace'){
       this.displayval = this.displayval.slice(0, -1);
+      if(this.displayval == null || this.displayval == "") {
+        this.calcval = '';
+      }
     }
     else if(this.calcval !== null && this.calcval !== "" && this.isEqual){
       this.displayval = this.calcval;
@@ -53,7 +94,7 @@ OnDisplay(clckval: string){
 GetTotalValue() {
 
     for (let i=0; i<(this.displayval.length); i++) {
-          if(this.displayval[i] == '+' || this.displayval[i] == '-' || this.displayval[i] == '*' || this.displayval[i] == '/') {
+          if(this.displayval[i] == '+' || this.displayval[i] == '-' || this.displayval[i] == '*' || this.displayval[i] == '/' || this.displayval[i] == '%') {
             if (this.operatorset) {
               this.nextcalcval = true;
               this.PerformCalc();
@@ -97,6 +138,14 @@ PerformCalc() {
       break;
     case '/' :
       this.subcalcval = this.var_1 / this.var_2;
+      break;
+    case '%' :
+      if(this.displayval.slice(-1) == '%' && this.var_2 == 0) {
+        this.subcalcval = this.var_1 / 100;
+      }
+      else {
+        this.subcalcval = (this.var_1 / 100) * this.var_2;
+      }
       break;
 
     default:
